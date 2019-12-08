4e233ff29fe661b5a1cba76782197adbb4e7e699
Johan Engwall <johan@holo.net>
2019-12-08 18:10:12 +0100

    Create Spring Boot project

---
+++ MockAllRest/src/test/java/com/engwall/restservice/MockAllRest/MockAllRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engwall.restservice.MockAllRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockAllRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
